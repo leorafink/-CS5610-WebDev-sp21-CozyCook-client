52830d00783c3a20ae5bb31c99bfe9a180bddc84
salehi-s <salehi.s@husky.neu.edu>
2021-03-26 15:02:52 -0400

    Add full recipe button to details page

--- src/components/details.js
+++ src/components/details.js
@@ -23,29 +23,36 @@ const Details = () => {
                 recipe && recipe[0] &&
                 <h1 className="wbdv-page-title">{recipe[0].label}</h1>
             }
-            <p>
-
-            </p>
             <div className = "container-fluid">
                 {
-                    recipe && recipe[0] && recipe[0].ingredientLines &&
-                        <div className = "container-fluid row">
-                            <span className = "container-fluid col-4">
-                                <img src = {recipe[0].image}/>
-                            </span>
+                    recipe && recipe[0] && recipe[0].ingredientLines && recipe[0].url && recipe[0].image &&
+                    <div className="container-fluid">
 
-                            <span className = "container-fluid col-8">
-                                <h3 className = "wbdv-ingredients-heading">Ingredients:</h3>
-                                <ul className="list-group">
-                                    {
-                                        recipe && recipe[0] && recipe[0].ingredientLines.map(ingredient =>
-                                            <li className="list-group-item" key={Math.random()}>
-                                                {ingredient}
-                                            </li>)
-                                    }
-                                </ul>
-                            </span>
-                        </div>
+                    <span className = "container-fluid">
+                        <img src = {recipe[0].image}/>
+                    </span>
+
+                    <span className = "container-fluid wbdv-details-area">
+                        <h3 className = "wbdv-ingredients-heading">Ingredients:</h3>
+                        <ul className="list-group">
+                            {
+                                recipe && recipe[0] && recipe[0].ingredientLines.map(ingredient =>
+                                    <li className="list-group-item" key={Math.random()}>
+                                        {ingredient}
+                                    </li>)
+                            }
+                        </ul>
+                    </span>
+
+                    <div className = "wbdv-full-recipe-button-area">
+                        <a href={recipe[0].url}>
+                            <button type="button"
+                                    className="btn btn-primary">
+                            See Full Recipe
+                            </button>
+                        </a>
+                    </div>
+                    </div>
                 }
             </div>
         </div>
