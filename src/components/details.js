66e2636c708b66422b432074dd5eada1e9b57dbf
salehi-s <salehi.s@husky.neu.edu>
2021-03-26 10:29:55 -0400

    Add back button to details

--- src/components/details.js
+++ src/components/details.js
@@ -1,16 +1,24 @@
 import React, {useState, useEffect} from 'react'
-import {useParams} from 'react-router-dom'
+import {useParams, useHistory} from 'react-router-dom'
 import recipeService from '../services/recipe-service'
 
 const Details = () => {
     const [recipe, setRecipe] = useState({})
     const {title, id} = useParams()
+    const history = useHistory()
+
     useEffect(() => {
         recipeService.findRecipeById(id)
             .then(recipe => setRecipe(recipe))
     }, [])
+
     return(
         <div className="container-xl">
+            <span className = "wbdv-go-back">
+                <i className = "fas fa-arrow-left fa-2x wbdv-action-icon"
+                   onClick = {() => history.goBack()}/>
+                Go Back
+            </span>
             {
                 recipe && recipe[0] &&
                 <h1 className="wbdv-page-title">{recipe[0].label}</h1>
