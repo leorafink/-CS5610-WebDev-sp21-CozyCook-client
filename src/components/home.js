2375de1fead33d731639348c1f014e7f66561ca0
salehi-s <salehi.s@husky.neu.edu>
2021-03-27 12:35:29 -0400

    Update Home Page Style

--- src/components/home.js
+++ src/components/home.js
@@ -3,14 +3,18 @@ import {Link} from "react-router-dom";
 
 const Home = () => {
     return(
-        <div className="container-xl">
-            <h1 className="wbdv-page-title">Home</h1>
-            <div className="wbdv-horizontal-center">
-                <Link to="/search">
-                    Search
+        <div className = "container-fluid wbdv-home-page-container">
+            <div className = "container-fluid wbdv-home-heading-container">
+                <h1 className = "wbdv-page-title">Home</h1>
+            </div>
+            <div className = "container-fluid wbdv-horizontal-center">
+                <Link to = "/search">
+                    <button type = "button"
+                            className = "btn btn-primary wbdv-home-button-search">
+                        Search
+                    </button>
                 </Link>
             </div>
-
         </div>
     )
 }
