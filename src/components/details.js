7426d6e69f03db74b6d3e2ca25d58be4a9d42ccc
AriellesMacBook <arielleslatus@login.khoury.neu.edu>
2021-03-27 16:37:20 -0400

    actually fixed image in details

--- src/components/details.js
+++ src/components/details.js
@@ -15,52 +15,38 @@ const Details = () => {
     return(
         <div className="container-fluid">
 
-                <div>
                     {
-                        recipe && recipe[0] &&
+                        recipe && recipe[0] && recipe[0].ingredientLines && recipe[0].url && recipe[0].image &&
                         <>
                         <h1 className="wbdv-page-heading">{recipe[0].label}</h1>
-                        <div className = "wbdv-go-back"
-                                              onClick = {() => history.goBack()}>
-                                            <i className = "fas fa-arrow-left fa-2x wbdv-action-icon"/>
-                                            Go Back
-                                        </div>
-                                        </>
-                    }
-                </div>
-            <div className = "container-fluid">
-                {
-                    recipe && recipe[0] && recipe[0].ingredientLines && recipe[0].url && recipe[0].image &&
-                    <div className="container-fluid">
-
-                        <div className = "container-fluid wbdv-details-image-container">
-                            <img className = "wbdv-details-image"
-                                 src = {recipe[0].image}/>
+                        <div className = "wbdv-go-back" onClick = {() => history.goBack()}>
+                            <i className = "fas fa-arrow-left fa-2x wbdv-action-icon"/>
+                            Go Back
                         </div>
-
+                        <div className="wbdv-details-image-container">
+                            <img className = "wbdv-details-image" src = {recipe[0].image}/>
+                        </div>
                         <div className = "container-fluid wbdv-details-area">
                             <h3 className = "wbdv-ingredients-heading">Ingredients:</h3>
                             <ul className="list-group">
                                 {
-                                    recipe && recipe[0] && recipe[0].ingredientLines.map(ingredient =>
-                                        <li className="list-group-item" key={Math.random()}>
-                                            {ingredient}
-                                        </li>)
-                                }
+                                    recipe[0].ingredientLines.map(ingredient =>
+                                          <li className="list-group-item" key={Math.random()}>
+                                              {ingredient}
+                                          </li>)
+                                 }
                             </ul>
                         </div>
-
                         <div className = "wbdv-full-recipe-button-area">
-                            <a href={recipe[0].url}>
-                                <button type="button"
-                                        className="btn btn-primary wbdv-details-button-recipe">
-                                See Full Recipe
-                                </button>
-                            </a>
+                             <a href={recipe[0].url}>
+                                  <button type="button" className="btn btn-primary wbdv-details-button-recipe">
+                                      See Full Recipe
+                                  </button>
+                             </a>
                         </div>
-                    </div>
-                }
-            </div>
+                        </>
+                    }
+
         </div>
     )
 }
