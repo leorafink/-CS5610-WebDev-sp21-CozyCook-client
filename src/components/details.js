7ae12639fc4f60adbe501548cfbbe45bce0a9c10
salehi-s <salehi.s@husky.neu.edu>
2021-03-27 14:08:07 -0400

    Adjust details formatting and styling

--- src/components/details.js
+++ src/components/details.js
@@ -13,46 +13,50 @@ const Details = () => {
     }, [])
 
     return(
-        <div className="container-xl">
-            <span className = "wbdv-go-back">
-                <i className = "fas fa-arrow-left fa-2x wbdv-action-icon"
-                   onClick = {() => history.goBack()}/>
-                Go Back
-            </span>
-            {
-                recipe && recipe[0] &&
-                <h1 className="wbdv-page-title">{recipe[0].label}</h1>
-            }
+        <div className="container-fluid">
+            <div className = "container-fluid wbdv-details-heading-area">
+                <div className = "wbdv-go-back"
+                      onClick = {() => history.goBack()}>
+                    <i className = "fas fa-arrow-left fa-2x wbdv-action-icon"/>
+                    Go Back
+                </div>
+                <span>
+                    {
+                        recipe && recipe[0] &&
+                        <h1 className="wbdv-page-title">{recipe[0].label}</h1>
+                    }
+                </span>
+            </div>
             <div className = "container-fluid">
                 {
                     recipe && recipe[0] && recipe[0].ingredientLines && recipe[0].url && recipe[0].image &&
                     <div className="container-fluid">
 
-                    <div className = "container-fluid wbdv-details-image-container">
-                        <img className = "wbdv-details-image"
-                             src = {recipe[0].image}/>
-                    </div>
+                        <div className = "container-fluid wbdv-details-image-container">
+                            <img className = "wbdv-details-image"
+                                 src = {recipe[0].image}/>
+                        </div>
 
-                    <span className = "container-fluid wbdv-details-area">
-                        <h3 className = "wbdv-ingredients-heading">Ingredients:</h3>
-                        <ul className="list-group">
-                            {
-                                recipe && recipe[0] && recipe[0].ingredientLines.map(ingredient =>
-                                    <li className="list-group-item" key={Math.random()}>
-                                        {ingredient}
-                                    </li>)
-                            }
-                        </ul>
-                    </span>
+                        <div className = "container-fluid wbdv-details-area">
+                            <h3 className = "wbdv-ingredients-heading">Ingredients:</h3>
+                            <ul className="list-group">
+                                {
+                                    recipe && recipe[0] && recipe[0].ingredientLines.map(ingredient =>
+                                        <li className="list-group-item" key={Math.random()}>
+                                            {ingredient}
+                                        </li>)
+                                }
+                            </ul>
+                        </div>
 
-                    <div className = "wbdv-full-recipe-button-area">
-                        <a href={recipe[0].url}>
-                            <button type="button"
-                                    className="btn btn-primary">
-                            See Full Recipe
-                            </button>
-                        </a>
-                    </div>
+                        <div className = "wbdv-full-recipe-button-area">
+                            <a href={recipe[0].url}>
+                                <button type="button"
+                                        className="btn btn-primary wbdv-details-button-recipe">
+                                See Full Recipe
+                                </button>
+                            </a>
+                        </div>
                     </div>
                 }
             </div>
