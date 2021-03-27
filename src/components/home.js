1589dadca4ec0d70c4a0b54b0e10b4eedfdd3518
salehi-s <salehi.s@husky.neu.edu>
2021-03-27 16:40:35 -0400

    Add logo to home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import {Link} from "react-router-dom";
+import mainImage from "./icon-restaurant-4.png"
 
 const Home = () => {
     return(
         <div className = "container-fluid wbdv-home-page-container">
             <h1 className = "wbdv-page-heading">Home</h1>
+            <div className = "container-fluid wbdv-home-image-container">
+                <img className = "img-fluid wbdv-home-image"
+                     src = {mainImage}/>
+            </div>
             <div className = "container-fluid wbdv-horizontal-center">
                 <Link to = "/search">
                     <button type = "button"
